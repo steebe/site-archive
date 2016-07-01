b8c096872cffadd90c5698c9b3cb6c5e419b26d5
Steve Bass <scbass@buffalo.edu>
2016-07-01 08:13:47 -0400

    Switched to master and added foodle

--- js/foodle.js
+++ js/foodle.js
@@ -1,12 +1,26 @@
-var $ = function()
+var $ = function(e)
 {
-  return document.getElementById();
-}
+  return document.getElementById(e);
 
+};
 
-var begin = function()
-{
+/*
+    begin()
+    Init of canvas state for gameplay.
+
+    TODO:
+        - Create random shapes/patterns in the canvas.
+        - Save the final canvas state as a JPG.
+*/
+var begin = function(){
   var canvas = $('myCan');
   var context = canvas.getContext("2d");
-  alert(canvas);
-}
+  context.fillStyle = "#2F4F4F";
+  context.fillRect(0,0,150,75);
+  alert("MADE IT!");
+
+};
+
+var trace = function(){
+
+};
